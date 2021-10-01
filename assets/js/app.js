7ff1d58959a8042e7b14da1130a6a7382edddbcb
Sarah Hollingsworth <sahhollingsworth@gmail.com>
2021-09-30 18:25:30 -0700

    Added HTML infrastructure for hourly timeblocks. Outlined remaining js work to meet project requirements.
    Acceptance criteria: When I scroll down then I am presented with timeblocks for standard business hours

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,20 @@
 var dateCurrent = moment();
 $("#currentDay").text(dateCurrent.format("dddd, MMMM Do"));
 
+// Listener for save button in each timeblock
+$(.save).onclick("click", saveBlock);
+
+// Function to saved timeblock input to local storage
+// function saveBlock() {
+//}
+// function saveBlocks() {
+//     localStorage.setItem("blocks", JSON.stringify(blocks));
+// }
+
+// Function to pull block data from local storage
+
+// Function to display block data from local storage
+
+// Function to check time at continuous intervals
+
+// Function to update color of blocks based on current time (past present future)
